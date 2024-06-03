8c2ea7b2d7e68e000c3e24f0f469a5d7fd69affc
m0nch1 <m0nch1.develop@gmail.com>
2024-06-04 02:33:48 +0900

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,8 +10,9 @@ export default defineNuxtConfig({
     preset: 'cloudflare-pages',
   },
   runtimeConfig: {
+    apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
     public: {
-      apiHost: NUXT_PUBLIC_API_HOST,
+      apiBase: '', // can be overridden by NUXT_PUBLIC_API_BASE environment variable
     }
   },
 })
