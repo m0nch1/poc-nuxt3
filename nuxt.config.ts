59ce183ccbf152b246b32d58c7b66dfed07ff9de
m0nch1 <m0nch1.develop@gmail.com>
2024-06-04 03:47:59 +0900

    doya

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,9 +12,9 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
-    apiSecret: "", // can be overridden by NUXT_API_SECRET environment variable
+    apiSecret: process.env.NUXT_API_SECRET, // can be overridden by NUXT_API_SECRET environment variable
     public: {
-      apiBase: "", // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+      apiBase: process.env.NUXT_PUBLIC_API_BASE, // can be overridden by NUXT_PUBLIC_API_BASE environment variable
       "graphql-client": {
         clients: {
           default: {
