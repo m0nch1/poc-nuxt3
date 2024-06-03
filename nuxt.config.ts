57ac7b533a54f024e0fec9416f73790a62482a77
m0nch1 <m0nch1.develop@gmail.com>
2024-06-04 03:39:41 +0900

    これは？

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
       "graphql-client": {
         clients: {
           default: {
-            host: "",
+            host: "https://localhost.example.com",
           },
         },
       },
